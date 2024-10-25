c5277ad0c9277cee77b5e07a743d50b6e0e2c221
nastyaland <132244161+nastyaland@users.noreply.github.com>
2024-10-24 21:26:54 -0400

    Create Lab1Part1.c

---
+++ Lab1/Lab1Part1.c
@@ -0,0 +1,20 @@
+/* 
+    Definition: Print Message with multiple lines and Symbols
+    Author: Anastasia Dimov 
+    Date: January 13, 2022
+*/ 
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("C uses escape sequences for a variety of purposes.\n");
+    printf("Some common ones are:\n");
+    printf("to print \", use \\\"\n");
+    printf("to print \\, use \\\\\n");
+    printf("to jump to a new line, use \\n");
+
+
+
+    return 0;
+}
